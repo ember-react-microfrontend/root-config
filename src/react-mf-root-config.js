cb1e94830db72ee763c9b43ea86956e59f72a0bd
Stephen <stephen.c.weiss@gmail.com>
2021-04-05 16:48:12 -0500

    register ember app

--- src/react-mf-root-config.js
+++ src/react-mf-root-config.js
@@ -4,6 +4,7 @@ import {
   constructLayoutEngine,
 } from "single-spa-layout";
 import { registerApplication, start } from "single-spa";
+import { loadEmberApp } from "single-spa-ember";
 
 const routes = constructRoutes(document.querySelector("#single-spa-layout"), {
   loaders: {
@@ -15,7 +16,9 @@ const routes = constructRoutes(document.querySelector("#single-spa-layout"), {
 });
 const applications = constructApplications({
   routes,
-  loadApp: ({ name }) => System.import(name),
+  loadApp: ({ name }) => {
+    return System.import(name);
+  },
 });
 // Delay starting the layout engine until the styleguide CSS is loaded
 const layoutEngine = constructLayoutEngine({
@@ -24,7 +27,27 @@ const layoutEngine = constructLayoutEngine({
   active: false,
 });
 
-applications.forEach(registerApplication);
+applications.forEach((app) => {
+  if (app.name === "people") {
+    const peopleDomain = "http://localhost:4201/people";
+    // const peopleDomain = isLocal
+    //   ? "http://localhost:4201/people"
+    //   : "https://ember-micro-frontends.github.io/people";
+    registerApplication(
+      "people",
+      () => {
+        const appName = "people";
+        const appUrl = `${peopleDomain}/assets/people.js`;
+        const vendorUrl = `${peopleDomain}/assets/vendor.js`;
+        return loadEmberApp(appName, appUrl, vendorUrl);
+      },
+      (location) => location.pathname.startsWith("/people/") // why do i need a trailing slash and why does this break everything else??
+    );
+  } else {
+    registerApplication(app);
+  }
+});
+// register ember app separately
 
 System.import("@react-mf/styleguide").then(() => {
   // Activate the layout engine once the styleguide CSS is loaded
